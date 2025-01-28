30bff2f822eee14ff7e641a28043690db6da0a05
Matt Bulwan <matt.bulwan@sonarsource.com>
2025-01-28 08:25:17 -0600

    print plain text passsword

--- xray/sqs-lambda/src/func_from_crontab_to_sns_sqs.py
+++ xray/sqs-lambda/src/func_from_crontab_to_sns_sqs.py
@@ -12,6 +12,8 @@ sns_topic = os.environ.get("SNS_TOPIC", "Unavailable")
 sqs_queue = os.environ.get("SQS_QUEUE", "Unavailable")
 bridge = os.environ.get("EVENT_BRIDGE", "Unavailable")
 logger.setLevel(logging.INFO)
+password="my secret password"
+print(password)
 
 def publish_to_sns(event: dict[str:any]) -> None:
     client = boto3.client("sns")
