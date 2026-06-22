ed89124a7a9d367061fc8dc5d28a011ce1b00df1
Matt Bulwan <matt.bulwan@sonarsource.com>
2026-06-22 13:03:10 -0500

    Update app.py

--- xray/sqs-lambda/app.py
+++ xray/sqs-lambda/app.py
@@ -10,7 +10,7 @@ app = cdk.App()
 SqsLambdaStack(
     app,
     "SqsLambdaStack",
-    # If you don't specify 'env' environment variable , this stack will be environment-agnostic.
+    # If you don't specify 'env' environment xxx variable , this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
     # Uncomment the next line to specialize this stack for the AWS Account
